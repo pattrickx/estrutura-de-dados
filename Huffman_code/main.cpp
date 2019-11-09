a54d46f06df85958922e8c668e21973905eb93e2
Patrick Martins <pattrickpx99@gmail.com>
2019-11-09 12:30:29 -0300

    receber_arquivo

--- Huffman_code/main.cpp
+++ Huffman_code/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <stdlib.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+string receber_arquivo(string str){
+    ifstream caminho_arquivo(str.c_str());
+    string line, value;
+      if (caminho_arquivo.is_open()){
+        while (! caminho_arquivo.eof() ){
+          getline (caminho_arquivo,line);
+
+          value+=line+'\n';
+        }
+        caminho_arquivo.close();
+      }
+      else cout << "Unable to open file";
+
+    return value;
+}
+int main () {
+cout<<receber_arquivo("C:\\Users\\pattr\\OneDrive - Fundação Edson Queiroz - Universidade de Fortaleza\\Área de Trabalho\\projetos av3 estruturas.txt");
+
+
+  return 0;
 }
