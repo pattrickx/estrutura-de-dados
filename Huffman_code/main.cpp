fd6712f1112e825d4803d62568253cf4ba26361c
Patrick Martins <pattrickpx99@gmail.com>
2019-11-09 17:27:47 -0300

    arvore com dados cadastrados
    ajuste função de remover_primeiro
    funcionamento completo ate esse ponto

--- Huffman_code/main.cpp
+++ Huffman_code/main.cpp
@@ -31,13 +31,12 @@ bool buscar_no(lno* a, char c){
         if(a->caracter==c)
             return true;
         buscar_no(a->proximo,c);
-    }
-    return false;
+    }else return false;
 }
 bool buscar(lista *a, char c){
-    if(a->inicio)
+
 return buscar_no(a->inicio,c);
-    return false;
+
 }
 lno *criar_no(char c,int f,lno* an,lno* p,lno* e,lno* d){
     lno *a =(lno*)malloc(sizeof(lno));
@@ -51,19 +50,20 @@ lno *criar_no(char c,int f,lno* an,lno* p,lno* e,lno* d){
     return a;
 }
 void add(lno *a,lno *n){
-    if(!a)
-        a=n;
-    else if(a->frequencia<n->frequencia){
-        n->proximo=a;
-        a->anterior=n;
-        a=n;
-
-    }
-    add(a->proximo,n);
+    if(!a->proximo){
+        a->proximo=n;
+        n->anterior=a;
+        }
+    else if(a->frequencia>n->frequencia){
+        n->proximo=a->proximo;
+        a->proximo=n;
+        n->anterior=a;
+    }else add(a->proximo,n);
 
 
 }
 void add_em_ordem(lista *a, lno *n){
+
     if(!a->inicio)
         a->inicio=n;
     else if (a->inicio->frequencia>n->frequencia){
@@ -71,8 +71,7 @@ void add_em_ordem(lista *a, lno *n){
         a->inicio->anterior=n;
         a->inicio=n;
     }
-    else
-        add(a->inicio,n);
+    else add(a->inicio,n);
 
 }
 int quantidade(string s, char c){
@@ -106,33 +105,64 @@ string receber_arquivo(string str){
 
     return s;
 }
-void mostrar(lno *a){
+void mostrar(lno *a,int i){
 if(a){
-    cout<<a->caracter<<" quantidade: "<<a->frequencia<<endl;
-    mostrar(a->proximo);
+    cout<<"caractere: "<<a->caracter<<" quantidade: "<<a->frequencia<<" numero: "<<i++<<endl;
+    mostrar(a->proximo,i);
+
     }
 }
 void mostrarlista(lista *a){
-mostrar(a->inicio);
+    if(a->inicio)
+    mostrar(a->inicio,0);
 }
-void criar_arvore(lista *a){
- //   arvore(a->inicio);
+void remover_primeiro(lista *a){
+    if(!a->inicio->proximo)
+    a->inicio=NULL;
+    else{
+        a->inicio=a->inicio->proximo;
+        a->inicio->anterior=NULL;
+   }
+
+}
+void criar_arvore(lista *a,int i){
+    //cout<<i<<endl;
+    if(a->inicio->proximo){
+
+        lno *temp=criar_no('+',a->inicio->frequencia+a->inicio->proximo->frequencia,criar_no_vazio(),criar_no_vazio(),a->inicio,a->inicio->proximo);
+        remover_primeiro(a);
+        //cout<<"teste"<<endl;
+        remover_primeiro(a);
+        add_em_ordem(a,temp);
+       // mostrarlista(a);
+        criar_arvore(a,++i);
+    }
+}
+void mostrar_arvore(lno *a){
+    if(a){
+        cout<<"caractere: "<<a->caracter<<" quantidade: "<<a->frequencia<<endl;
+        mostrar_arvore(a->esquerda);
+        mostrar_arvore(a->direita);
+    }
+
 }
 int main () {
-//string s =receber_arquivo("C:\\Users\\pattr\\OneDrive - Fundação Edson Queiroz - Universidade de Fortaleza\\Área de Trabalho\\projetos av3 estruturas.txt");
-string s =receber_arquivo("teste.txt");
+string s =receber_arquivo("C:\\Users\\pattr\\OneDrive - Fundação Edson Queiroz - Universidade de Fortaleza\\Área de Trabalho\\projetos av3 estruturas.txt");
+//string s =receber_arquivo("teste.txt");
 lista *l=criar_vazia();
-
+cout<<s;
 char value[s.length()];
     strcpy(value, s.c_str());
-    for(int i =0; i<sizeof(value);i++)
-        cout<<value[i]<<endl;
 for(int i =0; i<sizeof(value)-1;i++){
-        if(buscar(l,value[i])==false){
-            add_em_ordem(l,criar_no(value[i],quantidade(s,value[i]),criar_no_vazio(),criar_no_vazio(),criar_no_vazio(),criar_no_vazio()));
-        }
+        if(!buscar(l,value[i])){
+            lno *t=criar_no(value[i],quantidade(s,value[i]),criar_no_vazio(),criar_no_vazio(),criar_no_vazio(),criar_no_vazio());
+            add_em_ordem(l,t);
+            }
+
 }
-mostrarlista(l);
-//criar_arvore(l);
+
+//mostrarlista(l);
+criar_arvore(l,0);
+mostrar_arvore(l->inicio);
   return 0;
 }
